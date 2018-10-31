e806943f4f3df1ab7b50519ff061a2030b2af506
DhanashreeMohite <35923348+DhanashreeMohite@users.noreply.github.com>
2018-10-31 10:23:05 +0530

    Update ExponentialSeries.s

--- Assignment4/ExponentialSeries.s
+++ Assignment4/ExponentialSeries.s
@@ -3,23 +3,22 @@
 	 ENTRY 
 __main  FUNCTION
 
-	VMOV.F32 S1, #4				;x-Number to find e^x
-	VMOV.F32 S2, #5			;No. of terms been considered in e^x expansion
-	VMOV.F32 S3, #1				;counter for no. of terms
-	VMOV.F32 S4, #1				;temp
-	VMOV.F32 S5, #1				;result
-	VMOV.F32 S7, #1				;register to hold one
-Loop 
-	 VCMP.F32 S2, S3			;Comparison done for excuting taylor series expansion of e^x for s2 number of terms
-	 VMRS.F32 APSR_nzcv,FPSCR	;Used to copy fpscr to apsr
-	 BLT stop;
-	 VDIV.F32 S6, S1, S3		;temp1=x/count
-	 VMUL.F32 S4, S4, S6		;temp=temp*temp1;
-	 VADD.F32 S5, S5, S4		;result=result+temp;
-	 VADD.F32 S3, S3, S7		;incrementing count by 1
-
-	 B Loop; 
-	 
+		VMOV.F32 S0, #4 	;'x' in e^x
+		VMOV.F32 S1, #5 	;no. of terms
+		VMOV.F32 S2, #1 	;result
+		VMOV.F32 S3, #1 	;constant used for inreament
+		VMOV.F32 S4, #1 	;counter which is used for division also
+		VMOV.F32 S6, #1 
+LOOP
+		VCMP.F32 S1, S4
+		VMRS.F32 APSR_nzcv,FPSCR	;Used to copy fpscr to apsr
+		BLT stop;
+		VDIV.F32 S5,S0,S4			;S5 = (S0/S4) 
+		VMUL.F32 S6,S5,S6			;S6 = (S0/S4)*S6
+		VADD.F32 S2, S2, S6			;S2 = S2+((S0/S4)*S6)	
+		VADD.F32 S4, S4, S3			;incrementing count by 1
+		B LOOP;
+		
 stop B stop ; stop program
 	 ENDFUNC
 	 END
